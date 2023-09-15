6d8acd221b1d59cfc69e1cd86cf4ec0ac7e1ed8b
ScottBot10 <46166818+ScottBot10@users.noreply.github.com>
2023-09-15 18:01:32 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,127 @@
+# GitHub backup script
+Backs up GitHub data for multiple users by using GitHub's [migration API](https://docs.github.com/en/rest/migrations/users). Probably won't work to back up organisations.
+
+## Usage
+This script requires Python 3. If you want to use config in yaml, you will need the [PyYAML](https://pypi.org/project/PyYAML/) library. You can install it like this:
+```shell
+pip install PyYAML
+```
+To run the script run:
+```shell
+python github_backup.py
+```
+or
+```shell
+python github_backup.py /path/to/config.yml
+```
+### Config
+Config can be in either a YAML or a json format. Use the `CONFIG` variable near the top of the script to embed config into the script. If you do so, other sources of config will be ignored.
+
+yaml:
+```yaml
+logging:
+  version: 1
+  formatters:
+    brief:
+      format: '%(levelname)s:%(message)s'
+    precise:
+      format: '%(levelname)s:%(asctime)s:%(name)s:%(threadName)s:%(message)s'
+  handlers:
+    console:
+      class: logging.StreamHandler
+      level: INFO
+      formatter: brief
+      stream: ext://sys.stdout
+    file:
+      class: logging.FileHandler
+      level: DEBUG
+      formatter: precise
+      filename: backup.log
+      encoding: utf-8
+  loggers:
+    github_backup:
+      level: DEBUG
+      handlers: [console, file]
+global:
+  outfile: "./backups/{username}/{datetime:%d%m%y_%H%M%S}.tar.gz"
+users:
+  - token: !ENV GH_TOKEN
+    check_time: 10
+  - token: ghp_*************************************
+    outfile: "/other/location/{username}.tar.gz"
+    exclude_git_data: True
+```
+json:
+```json
+{
+  "logging": {
+    "version": 1,
+    "formatters": {
+      "brief": {
+        "format": "%(levelname)s:%(message)s"
+      },
+      "precise": {
+        "format": "%(levelname)s:%(asctime)s:%(name)s:%(threadName)s:%(message)s"
+      }
+    },
+    "handlers": {
+      "console": {
+        "class": "logging.StreamHandler",
+        "level": "INFO",
+        "formatter": "brief",
+        "stream": "ext://sys.stdout"
+      },
+      "file": {
+        "class": "logging.FileHandler",
+        "level": "DEBUG",
+        "formatter": "precise",
+        "filename": "backup.log"
+      }
+    },
+    "loggers": {
+      "github_backup": {
+        "level": "DEBUG",
+        "handlers": [
+          "console",
+          "file"
+        ]
+      }
+    }
+  },
+  "global": {
+    "outfile": "./backups/{username}/{datetime:%Y-%m-%dT%H%M%S}.tar.gz"
+  },
+  "users": [
+    {
+      "token": "ghp_*************************************"
+    },
+    {
+      "token": "ghp_*************************************",
+      "outfile": "/other/location/{username}.tar.gz",
+      "exclude_git_data": true
+    }
+  ]
+}
+```
+The `global` section will apply to every user, but you can override the values
+for individual users. You can use the `!ENV` constructor in the YAML config to reference environment variables.
+
+#### User
+The config for each user is in a list under the `users` key. For each user, the following options can be used:
+
+- `token` - Your GitHub [personal access token](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens). Cannot go in `global`. ***Required***
+- `outfile` - The file name to save the backup as, including the `.tar.gz`. Can use formatting syntax including: `{username}`, `{datetime}` (of backup finish; see [format codes](https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior)) and `{id}` (of backup). If it is set to None, then the script will not download the backup. It will still check its state.
+- `check_time` - How long to wait inbetween checking the state of the backup in seconds. If it is set to None, the script won't query the state of the backup and so won't automatically download it. Default: `30`
+- `affiliation` - Comma separated values. Options: `owner`, `collaborator` and `organization_member`. Default: `owner`
+- `visibility` - Limits repositories to back up to ones with a certain visibility. Options: `all`, `public`, `private`. Default: `all`
+- `exclude_repos` - A string or list of strings that are the full names (`username/repo_name`) of repositories that you don't want to back up.
+- `lock_repositories` - Lock the repositories being backed up. Default: `False`
+- `exclude_metadata` - Whether metadata, such as __, should be excluded. Default: `False`
+- `exclude_git_data` - Whether the repositories' git data should be excluded. Default: `False`
+- `exclude_attachments` - Do not include attachments. Default: `False`
+- `exclude_releases` - Do not include releases. Default: `False`
+- `exclude_owner_projects` - Whether projects owned by the user should be excluded. Default: `False`
+- `org_metadata_only` - Whether the backup should only include metadata (will ignore the exclude flags.). Default: `False`
+
+#### Logging
+This script uses Python's [logging](https://docs.python.org/3/library/logging.html) module to log output. You can add logging config under the `logging` key using the [dictConfig() schema](https://docs.python.org/3/library/logging.config.html#configuration-dictionary-schema). The variable, `LOGGER_NAME`, at the top of the script will be used as the name of the logger (default `'github_backup'`), so make sure to have the same name in the `loggers` part of your config.
---
+++ github_backup.py
@@ -0,0 +1,228 @@
+import json
+import os
+import shutil
+import threading
+import time
+from datetime import datetime
+from urllib.parse import urlencode
+import logging
+import logging.config
+from urllib.request import urlopen, Request
+
+try:
+    from dotenv import load_dotenv
+
+    load_dotenv()
+except ImportError:
+    pass
+
+# Add config here to use instead of the external file.
+# If you do, only this config will be used.
+CONFIG = """
+
+"""
+DEFAULT_CONFIG_FILE = "config.yml"
+LOGGER_NAME = "github_backup"
+
+API = "https://api.github.com"
+HEADERS = {"Accept": "application/vnd.github+json"}
+
+
+# TODO: add error logging
+def request(url, logger, params=None, headers=None, json_data=None):
+    if params:
+        url += "?" + urlencode(params)
+    if headers is None:
+        headers = {}
+    if json_data is not None:
+        json_data = json.dumps(json_data).encode("utf-8")
+        headers = dict(**headers, **{"Content-Type": "application/json"})
+    with urlopen(Request(url, headers=headers, data=json_data)) as r:
+        return json.loads(r.read())
+
+
+# try to use the PyYAML library. If it is not available, use the json library
+try:
+    import yaml as _yaml
+
+
+    class YAMLLoader(_yaml.SafeLoader):
+        @staticmethod
+        def _env_var_constructor(loader, node):
+            """
+            Constructor for inserting the values of environment variable to a yaml file.
+            Can either be a string or a list of length two
+            with the first value being the name of the environment variable
+            and the second being a default value for if the variable doesn't exist.
+            """
+
+            default = None
+
+            if node.id == 'scalar':
+                key = str(loader.construct_scalar(node))
+            else:
+                seq = loader.construct_sequence(node)
+
+                if len(seq) > 1:
+                    key = seq[0]
+                    default = seq[1]
+                else:
+                    key = seq[0]
+
+            return os.getenv(key, default)
+
+        def __init__(self, stream):
+            super().__init__(stream)
+            self.add_constructor("!ENV", self._env_var_constructor)
+            self.add_constructor("!env", self._env_var_constructor)
+
+
+    def load_string(s):
+        return _yaml.load(s, YAMLLoader)
+
+
+    load_file = load_string
+except ImportError:
+    def load_string(s):
+        return json.loads(s)
+
+
+    def load_file(f):
+        return json.load(f)
+
+
+class User:
+    """
+    Class representing a user that has a token. Encapsulates all the data of a user to easily pass to a thread.
+    """
+
+    def __init__(self, global_config: dict, user_config: dict, logger: logging.Logger):
+        self.logger = logger
+
+        self.token = user_config.get('token')
+        if self.token is None:
+            raise ValueError('token is required')
+
+        self.outfile = self._config_val('outfile', None, global_config, user_config)
+        self.check_time = self._config_val('check_time', 30, global_config, user_config)
+        self.affiliation = self._config_val('affiliation', 'owner', global_config, user_config)
+        self.visibility = self._config_val('visibility', 'all', global_config, user_config)
+        self.exclude_repos = self._config_val('exclude_repos', set(), global_config, user_config)
+        if isinstance(self.exclude_repos, str):
+            self.exclude_repos = {self.exclude_repos}
+        self.lock_repositories = self._config_val('lock_repositories', False, global_config, user_config)
+        self.exclude_metadata = self._config_val('exclude_metadata', False, global_config, user_config)
+        self.exclude_git_data = self._config_val('exclude_git_data', False, global_config, user_config)
+        self.exclude_attachments = self._config_val('exclude_attachments', False, global_config, user_config)
+        self.exclude_releases = self._config_val('exclude_releases', False, global_config, user_config)
+        self.exclude_projects = self._config_val('exclude_owner_projects', False, global_config, user_config)
+        self.org_metadata_only = self._config_val('org_metadata_only', False, global_config, user_config)
+
+        self.headers = dict(**HEADERS, Authorization=f"Bearer {self.token}")
+
+        self.repos = list(self.get_repos()) if not self.org_metadata_only else []
+        self.id, self.username = self.start_migration()
+
+    @staticmethod
+    def _config_val(var, default, global_config, user_config):
+        if var in user_config:
+            return user_config[var]
+        if var in global_config:
+            return global_config[var]
+        return default
+
+    def get_repos(self):
+        js = request(f"{API}/user/repos", self.logger, headers=self.headers, params={
+            'affiliation': self.affiliation,
+            'visibility': self.visibility
+        })
+        self.logger.debug(f"Got repository info")
+        return {repo.get('full_name') for repo in js} - self.exclude_repos
+
+    def start_migration(self):
+
+        data = {
+            "repositories": self.repos,
+            "lock_repositories": self.lock_repositories,
+            "exclude_metadata": self.exclude_metadata,
+            "exclude_git_data": self.exclude_git_data,
+            "exclude_attachments": self.exclude_attachments,
+            "exclude_releases": self.exclude_attachments,
+            "exclude_owner_projects": self.exclude_projects
+        }
+        if self.org_metadata_only:
+            data["org_metadata_only"] = self.org_metadata_only
+        js = request(f"{API}/user/migrations", self.logger, headers=self.headers, json_data=data)
+        mid, name = js["id"], js["owner"]["login"]
+        self.logger.info(f"Started backup {mid} for user: {name}")
+        self.logger.debug(js)
+        return mid, name
+
+    def get_state(self):
+        js = request(f"{API}/user/migrations/{self.id}", self.logger, headers=self.headers)
+        state = js.get('state')
+        self.logger.debug(f"Got state: '{state}'")
+        return state, js.get('updated_at')
+
+    def check(self):
+        self.logger.info(f"Waiting for {self.username}'s backup to complete...")
+        while True:
+            time.sleep(self.check_time)
+            state, dt = self.get_state()
+            if state == "exported":
+                self.logger.info(f"Backup for {self.username} created")
+                if self.outfile is not None:
+                    dt = datetime.strptime(dt, "%Y-%m-%dT%H:%M:%S.%f%z")
+                    self.save_backup(dt)
+                    self.logger.info(f"Saved backup for {self.username}")
+                break
+            elif state == "failed":
+                self.logger.error(f"Backup for {self.username} failed!")
+                break
+
+    def save_backup(self, dt):
+        filename = self.outfile.format(**{
+            "username": self.username,
+            "datetime": dt,
+            "id": self.id
+        })
+        self.logger.debug(f"Downloading {self.username}'s backup to {filename}")
+
+        with (
+            urlopen(Request(f"{API}/user/migrations/{self.id}/archive", headers=self.headers)) as r,
+            open(filename, 'wb') as f
+        ):
+            shutil.copyfileobj(r, f)
+
+
+def main(args):
+    if CONFIG.strip():  # If the config variable is empty
+        config = load_string(CONFIG)
+    else:
+        config_file = args[0] if args else DEFAULT_CONFIG_FILE
+        with open(config_file) as f:
+            config = load_file(f)
+    global_config = config.get('global', {})
+    logging_config = config.get('logging')
+
+    if logging_config is not None:
+        logging.config.dictConfig(logging_config)
+    logger = logging.getLogger(LOGGER_NAME)
+    logger.debug('Logger initialized')
+
+    users = [User(global_config, user_config, logger) for user_config in config['users']]
+    threads = []
+    for user in users:
+        if user.check_time > 0:
+            thread = threading.Thread(target=User.check, args=(user,))
+            threads.append(thread)
+            thread.start()
+
+    for thread in threads:
+        thread.join()
+
+
+if __name__ == "__main__":
+    import sys
+
+    main(sys.argv[1:])
